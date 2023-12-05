e4d78b2155f911602e7bd79b0015bc1e4a33b1cf
Valentina Ortiz <svalentinaog10@gmail.com>
2023-12-04 22:02:12 -0500

    ES/EN

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,29 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import { changeLanguage } from "i18next";
+import { useTranslation } from "react-i18next";
 function App() {
   const [count, setCount] = useState(0);
-
+  const [t, i18] = useTranslation();
   return (
     <>
       <div>
+        <h2>{t("Welcome to React")}</h2>
+        <button
+          onClick={() => {
+            changeLanguage("en");
+          }}
+        >
+          EN
+        </button>
+        <button
+          onClick={() => {
+            changeLanguage("es");
+          }}
+        >
+          ES
+        </button>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
