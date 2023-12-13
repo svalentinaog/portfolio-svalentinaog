14ceeb8a00c7bb91f08d327edc22b2d72e7e050a
Valentina Ortiz <116057704+svalentinaog@users.noreply.github.com>
2023-12-12 19:55:37 -0500

    Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import BaseLayout from '../components/BaseLayout/BaseLayout'
 import Entry from '../components/Entry'
+import AboutMe from '../components/AboutMe'
 
 export default function Home() {
   return (
     <BaseLayout>
         <Entry />
       <div className='landing-page'>
+        <AboutMe />
       </div>
     </BaseLayout>
   )
