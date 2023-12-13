cf53485b8a0d931f1a54416679b027b7a3af13a0
Valentina Ortiz <116057704+svalentinaog@users.noreply.github.com>
2023-12-13 00:27:13 -0500

    Update

--- src/components/BaseLayout/Footer.tsx
+++ src/components/BaseLayout/Footer.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-function Footer() {
+export default function Footer() {
+
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <p>All rights reserved © 2023</p>
+        <img
+          src="/images/medusa.png"
+          alt="isotipo"
+        />
+    </footer>
+  );
 }
-
-export default Footer
