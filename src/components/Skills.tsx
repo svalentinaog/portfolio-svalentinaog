56a0b2d02353085909386f64ef9929624aee7a2c
Valentina Ortiz <116057704+svalentinaog@users.noreply.github.com>
2024-01-09 13:58:36 -0500

    update

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,19 +7,29 @@ export default function SKills() {
   const skillsData = [
     { name: 'HTML5', image: 'html-5.png' },
     { name: 'CSS3', image: 'css-3.png' },
+    { name: 'JavaScript', image: 'js.png' },
+    { name: 'TypeScript', image: 'ts.png' },
+    { name: 'React.js', image: 'reactjs.png' },
+    { name: 'Redux Toolkit', image: 'redux.png' },
+    { name: 'NextJS', image: 'nextjs.png' },
     { name: 'Sass', image: 'sass.png' },
     { name: 'Bootstrap', image: 'bootstrap.png' },
     { name: 'Tailwind CSS', image: 'tailwindcss.png' },
-    { name: 'JavaScript', image: 'js.png' },
-    { name: 'TypeScript', image: 'ts.png' },
-    { name: 'React', image: 'reactjs.png' },
-    { name: 'NextJS', image: 'nextjs.png' },
-    { name: 'Redux Toolkit', image: 'redux.png' },
     { name: 'Material UI', image: 'materialui.png' },
-    { name: 'Node', image: 'nodejs.png' },
-    { name: 'Express', image: 'expressjs.png' },
-    { name: 'Sequelize', image: 'sequelize.png' },
+    { name: 'Font Awesome', image: 'font-awesome.png' },
+    { name: 'Node.js', image: 'nodejs.png' },
+    { name: 'Express.js', image: 'express-js.png' },
+    { name: 'SQL', image: 'sql.png' },
     { name: 'PostgreSQL', image: 'postgresql.png' },
+    { name: 'Sequelize', image: 'sequelize.png' },
+    { name: 'Git', image: 'git.png' },
+    { name: 'GitHub', image: 'github.png' },
+    { name: 'Postman', image: 'postman.svg' },
+    { name: 'Wordpress', image: 'wordpress.png' },
+    { name: 'Figma', image: 'figma.png' },
+    { name: 'Adobe Xd', image: 'xd.png' },
+    { name: 'Adobe Illustrator', image: 'illustrator.png' },
+    { name: 'Adobe Photoshop', image: 'photoshop.png' },
   ];
 
   return (
@@ -28,12 +38,14 @@ export default function SKills() {
       <p>{lg("skills2")}</p>
       <div className='all-skills'>
         {skillsData.map((skill, index) => (
-          <p key={index}>
-            <span className='icon-skill'>
+          <div key={index} className='content-skill'>
+            <div className='icon-skill'>
               <img className='' src={`images/${skill.image}`} alt="about" />
+            </div>
+            <span>
+              {skill.name}
             </span>
-            {/* {skill.name} */}
-          </p>
+          </div>
         ))}
       </div>
     </div>
