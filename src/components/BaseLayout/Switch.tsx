2b8d95eb1ab82f28c5f653fffff23f78c2469e61
Valentina Ortiz <116057704+svalentinaog@users.noreply.github.com>
2024-01-14 16:39:23 -0500

    Update

--- src/components/BaseLayout/Switch.tsx
+++ src/components/BaseLayout/Switch.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
 import { useTheme } from '../context/ThemeContext';
 
 function Switch() {
