276ee211f1579b5828693ca335d09c14cf96457a
Valentina Ortiz <116057704+svalentinaog@users.noreply.github.com>
2024-01-15 20:57:28 -0500

    update select

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,9 +1,116 @@
 import React from 'react';
+import { Form, InputGroup } from 'react-bootstrap';
+import { useTranslation } from "react-i18next";
 
 const ContactMe: React.FC = () => {
-    return (
-        <div id='section5' className='container-contactme paddings'>CONTACT ME</div>
-      );
+  const [lg] = useTranslation();
+
+  return (
+    <div id='section5' className='container-contactme paddings'>
+      <div className="text-content-contactme">
+        <h1>{lg("contact1")}</h1>
+        <div>
+          <p>{lg("contact2")}</p>
+        </div>
+        <div className='social-media-container'>
+          <a href="https://github.com/svalentinaog">
+            <div className="icon-github icons">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 496 512"
+              >
+                <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+              </svg>
+              {/* <FontAwesomeIcon icon={faGitHub} /> */}
+            </div>
+          </a>
+
+          <a href="https://www.linkedin.com/in/svalentinaog">
+            <div className="icon-linkedin icons">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z" />
+              </svg>
+            </div>
+          </a>
+
+          <a href="mailto:svalentinaog10@gmail.com">
+            <div className="icon-email icons">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" />
+              </svg>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className="container-formContact">
+        <form
+          className="form-wrapper custom-form-wrapper-contact"
+        >
+          <label htmlFor="user_name">Nombre completo</label>
+          <InputGroup className="form-input-group">
+            <InputGroup.Text id="basic-addon1" className="form-input-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 512 512"
+              >
+                <path d="M406.5 399.6C387.4 352.9 341.5 320 288 320H224c-53.5 0-99.4 32.9-118.5 79.6C69.9 362.2 48 311.7 48 256C48 141.1 141.1 48 256 48s208 93.1 208 208c0 55.7-21.9 106.2-57.5 143.6zm-40.1 32.7C334.4 452.4 296.6 464 256 464s-78.4-11.6-110.5-31.7c7.3-36.7 39.7-64.3 78.5-64.3h64c38.8 0 71.2 27.6 78.5 64.3zM256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-272a40 40 0 1 1 0-80 40 40 0 1 1 0 80zm-88-40a88 88 0 1 0 176 0 88 88 0 1 0 -176 0z" />
+              </svg>
+            </InputGroup.Text>
+            <Form.Control
+              className="form-input"
+              name="user_name"
+              placeholder="Joe Duplantier"
+              aria-label="nombre-completo"
+              aria-describedby="basic-addon1"
+              id="user_name"
+            />
+          </InputGroup>
+
+          <label htmlFor="user_email">Correo electrónico</label>
+          <InputGroup className="form-input-group">
+            <InputGroup.Text id="basic-addon1" className="form-input-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 512 512"
+              >
+                <path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" />
+              </svg>
+            </InputGroup.Text>
+            <Form.Control
+              className="form-input"
+              placeholder="joeduplantier@gmail.com"
+              name="user_email"
+              aria-label="joeduplantier@gmail.com"
+              aria-describedby="basic-addon1"
+              id="user_email"
+            />
+          </InputGroup>
+
+          <label htmlFor="user_message">Mensaje</label>
+          <InputGroup className="form-input-group">
+            <Form.Control
+              className="form-textarea"
+              as="textarea"
+              name="user_message"
+              rows={3}
+              placeholder="Ingrese su mensaje..."
+              id="user_message"
+            />
+          </InputGroup>
+
+          <button type="submit" className="form-submit">
+            Enviar mensaje
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default ContactMe;
+
