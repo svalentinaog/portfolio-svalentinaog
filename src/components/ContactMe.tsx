0174398843473d4a107da9e12c2e516145af78fa
Valentina Ortiz <116057704+svalentinaog@users.noreply.github.com>
2024-01-15 21:41:56 -0500

    Update form-contact

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Form, InputGroup } from 'react-bootstrap';
 import { useTranslation } from "react-i18next";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const ContactMe: React.FC = () => {
   const [lg] = useTranslation();
@@ -36,13 +38,13 @@ const ContactMe: React.FC = () => {
             </div>
           </a>
 
-          <a href="mailto:svalentinaog10@gmail.com">
+          {/* <a href="mailto:svalentinaog10@gmail.com">
             <div className="icon-email icons">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                 <path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" />
               </svg>
             </div>
-          </a>
+          </a> */}
         </div>
       </div>
       <div className="container-formContact">
@@ -52,13 +54,14 @@ const ContactMe: React.FC = () => {
           <label htmlFor="user_name">{lg("contact3")}</label>
           <InputGroup className="form-input-group">
             <InputGroup.Text id="basic-addon1" className="form-input-icon">
-              <svg
+              {/* <svg
                 xmlns="http://www.w3.org/2000/svg"
                 height="1em"
                 viewBox="0 0 512 512"
               >
                 <path d="M406.5 399.6C387.4 352.9 341.5 320 288 320H224c-53.5 0-99.4 32.9-118.5 79.6C69.9 362.2 48 311.7 48 256C48 141.1 141.1 48 256 48s208 93.1 208 208c0 55.7-21.9 106.2-57.5 143.6zm-40.1 32.7C334.4 452.4 296.6 464 256 464s-78.4-11.6-110.5-31.7c7.3-36.7 39.7-64.3 78.5-64.3h64c38.8 0 71.2 27.6 78.5 64.3zM256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-272a40 40 0 1 1 0-80 40 40 0 1 1 0 80zm-88-40a88 88 0 1 0 176 0 88 88 0 1 0 -176 0z" />
-              </svg>
+              </svg> */}
+              <FontAwesomeIcon icon={faUser} />
             </InputGroup.Text>
             <Form.Control
               className="form-input"
@@ -73,13 +76,16 @@ const ContactMe: React.FC = () => {
           <label htmlFor="user_email">{lg("contact4")}</label>
           <InputGroup className="form-input-group">
             <InputGroup.Text id="basic-addon1" className="form-input-icon">
-              <svg
+              {/* <svg
                 xmlns="http://www.w3.org/2000/svg"
                 height="1em"
                 viewBox="0 0 512 512"
               >
                 <path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" />
-              </svg>
+              </svg> */}
+
+            <FontAwesomeIcon icon={faEnvelope} />
+
             </InputGroup.Text>
             <Form.Control
               className="form-input"
